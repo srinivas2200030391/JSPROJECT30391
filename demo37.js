048d2a06d296d1cd74eb2f3619d64f526050b0ed
srinivas2200030391 <116977437+srinivas2200030391@users.noreply.github.com>
2024-01-15 14:27:24 +0530

    Add files via upload

---
+++ demo37.js
@@ -0,0 +1,15 @@
+let n = parseInt(prompt("Enter Input"), 0);
+let digit = 0;
+let sum = 0;
+let temp = n;
+while (n != 0) {
+  digit = parseInt(n % 10);
+  //sum += Math.pow(digit, 3);
+
+  n = parseInt(n / 10);
+}
+if (sum == temp) {
+  document.writeln(`${temp} is an Amstrong number`);
+} else {
+  document.writeln(`${temp} is not an Amstrong number`);
+}
